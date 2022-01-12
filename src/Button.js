e16f476897507e9c0bdb4bc1c75195d3254f0a63
Veselin <vesselin60@gmail.com>
2022-01-12 10:02:24 +0200

    initial commit: context provider

---
+++ src/Button.js
@@ -0,0 +1,7 @@
+
+
+const Button = () => {
+    return <button className="light-theme">Switch Theme</button>;
+};
+
+export default Button;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="light-theme">
+            <h2>Light Theme</h2>
+        </header>
+    );
+};
+
+export default Header;
